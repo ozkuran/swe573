112631b11f3e54c253b0dd321c82a46402e2fdde
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2016-04-20 22:35:48 +0300

    Added POJOs for JPA

---
+++ source/src/dto/TransactiondetailsEntity.java
@@ -0,0 +1,12 @@
+package dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by MahmutAli on 4/20/2016.
+ */
+@Entity
+@Table(name = "transactiondetails", schema = "bukoop", catalog = "")
+public class TransactiondetailsEntity {
+}
---
+++ source/src/dto/UnitsEntity.java
@@ -0,0 +1,12 @@
+package dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by MahmutAli on 4/20/2016.
+ */
+@Entity
+@Table(name = "units", schema = "bukoop", catalog = "")
+public class UnitsEntity {
+}
