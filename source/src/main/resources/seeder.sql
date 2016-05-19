ed0d9de43e0a62fc27e7cb847af01a44604c7eee
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2016-05-19 20:28:32 +0300

    Database Seeder Script
    Database Seeder Script Created. role and product_type tables data
    seeded. Related to issue #45.

---
+++ source/src/main/resources/seeder.sql
@@ -0,0 +1,25 @@
+INSERT INTO role (id, name)
+     VALUES (1,"admin");
+
+INSERT INTO role (id, name)
+     VALUES (2,"worker");
+
+INSERT INTO role (id, name)
+     VALUES (3,"member");
+
+INSERT INTO role (id, name)
+     VALUES (4,"nonmember");
+
+INSERT INTO product_type (id, name)
+     VALUES (1,"zeytin");
+
+INSERT INTO product_type (id, name)
+     VALUES (2,"peynir");
+
+INSERT INTO product_type (id, name)
+     VALUES (3,"tursu");
+
+INSERT INTO product_type (id, name)
+     VALUES (4,"süt");
+
+
