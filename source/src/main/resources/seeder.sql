efb4b7e8bdbdcd56212949249bd11a389625ec42
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2016-05-21 14:54:09 +0300

    Created Product and Products Page
    Created Product and Products Page. Closes issue #15 and improvements on
    issue #55.

--- source/src/main/resources/seeder.sql
+++ source/src/main/resources/seeder.sql
@@ -56,37 +56,40 @@ Toplanma yeri: Baraka (Kuzey Kampüs Shuttle durağının orada)
 Saat: 09.30
 Siz de gelmek isterseniz cansuplati@gmail.com adresine mutlaka e-mail atın ki yer durumunu ayarlayabilelim :)","http://www.bukoop.org/wp-content/uploads/2016/03/MAV%C4%B0C%C4%B0FTL%C4%B0KAF%C4%B0S.jpg");
 
+DELETE FROM product;
 
 DELETE FROM producer;
-INSERT INTO producer (id,name,photourl)
-VALUES (1, "Sındı Köyü Tarımsal Kalkınma Kooperatifi", "images/farm001.jpg");
+
 
 INSERT INTO producer (id,name,photourl)
-VALUES (2, "Urfa Kadın Kooperatifi", "images/farm002.jpg");
+VALUES (1, "Sındı Köyü Tarımsal Kalkınma Kooperatifi", "/images/farm001.jpg");
 
 INSERT INTO producer (id,name,photourl)
-VALUES (3, "Honaz Tarımsal Kalkınma Kooperatifi", "images/farm003.jpg");
+VALUES (2, "Urfa Kadın Kooperatifi", "/images/farm002.jpg");
 
 INSERT INTO producer (id,name,photourl)
-VALUES (4, "Vakıflı Köyü Tarımsal Kalkınma Kooperatifi", "images/farm001.jpg");
+VALUES (3, "Honaz Tarımsal Kalkınma Kooperatifi", "/images/farm003.jpg");
 
 INSERT INTO producer (id,name,photourl)
-VALUES (5, "Karaburun Kadın Kooperatifi", "images/farm002.jpg");
+VALUES (4, "Vakıflı Köyü Tarımsal Kalkınma Kooperatifi", "/images/farm001.jpg");
 
 INSERT INTO producer (id,name,photourl)
-VALUES (6, "Cevat Öztel", "images/farm006.jpg");
+VALUES (5, "Karaburun Kadın Kooperatifi", "/images/farm002.jpg");
+
+INSERT INTO producer (id,name,photourl)
+VALUES (6, "Cevat Öztel", "/images/farm006.jpg");
 
 DELETE FROM product;
 
 INSERT INTO product (id,description, package_type_id,producer_id,product_type_id,photourl,price)
-    VALUES (1,"Siyah Zeytin",1,1,1,"images/blackolive.jpg",10);
+    VALUES (1,"Siyah Zeytin",1,1,1,"/images/blackolive.jpg",10);
 
 INSERT INTO product (id,description, package_type_id,producer_id,product_type_id,photourl,price)
-VALUES (2,"Yeşil Zeytin",1,1,1,"images/greenolive.jpg",10);
+VALUES (2,"Yeşil Zeytin",1,1,1,"/images/greenolive.jpg",10);
 
 INSERT INTO product (id,description, package_type_id,producer_id,product_type_id,photourl,price)
-VALUES (3,"Süt",1,2,4,"images/milk.jpg",10);
+VALUES (3,"Süt",1,2,4,"/images/milk.jpg",10);
 
 INSERT INTO product (id,description, package_type_id,producer_id,product_type_id,photourl,price)
-VALUES (4,"Süt",2,3,4,"images/blackolive.jpg",10);
+VALUES (4,"Süt",2,3,4,"/images/blackolive.jpg",10);
 
